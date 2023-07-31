c6312824fe791782dee885a19b4f22df28b9e4ee
semyon <semyon.mihalich@gmail.com>
2023-07-31 14:50:11 +0500

    1. Quartz [#175122]
    Signed-off-by: semyon <semyon.mihalich@gmail.com>

---
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -0,0 +1,50 @@
+package ru.job4j.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class AlertRabbit  {
+
+
+    public int getTime() throws IOException {
+        Properties properties = new Properties();
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            properties.load(in);
+        }
+        return Integer.parseInt(properties.get("rabbit.interval").toString());
+    }
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        AlertRabbit alertRabbit = new AlertRabbit();
+        int time = alertRabbit.getTime();
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(time)
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+}
