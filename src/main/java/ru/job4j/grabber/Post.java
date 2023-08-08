3f6aaf58b097a91da856208c5266cbeed574fc64
semyon <semyon.mihalich@gmail.com>
2023-08-08 17:11:08 +0500

    2.2. Модель данных - Post. [#285211]
    Signed-off-by: semyon <semyon.mihalich@gmail.com>

---
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -0,0 +1,40 @@
+package ru.job4j.grabber;
+
+import java.time.LocalDateTime;
+
+public class Post {
+    int id;
+    String title;
+    String link;
+    String description;
+    LocalDateTime created;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Post post = (Post) o;
+
+        if (!link.equals(post.link)) return false;
+        return created.equals(post.created);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = link.hashCode();
+        result = 31 * result + created.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", link='" + link + '\''
+                +  ", description='" + description + '\''
+                + ", created=" + created
+                + '}';
+    }
+}
