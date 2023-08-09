f777d76d2c5519ff9ef9b3423bd12b1b79f605c8
semyon <semyon.mihalich@gmail.com>
2023-08-09 10:48:02 +0500

    2.1. Преобразование даты [#289476 #435488]
    Signed-off-by: semyon <semyon.mihalich@gmail.com>

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -2,10 +2,12 @@ package ru.job4j.grabber.utils;
 
 import java.time.LocalDateTime;
 
+import static java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+
 public class HabrCareerDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
-        return LocalDateTime.parse(parse.substring(0, 19));
+        return LocalDateTime.parse(parse, ISO_OFFSET_DATE_TIME);
     }
 }
